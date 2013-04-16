b0a6864e49196598a6f13db821fdb59276021439
traboukos <dstefanidis@gmail.com>
2013-04-16 18:28:50 +0300

    Add support for multiple facet selection

--- lib/spree/sunspot/search.rb
+++ lib/spree/sunspot/search.rb
@@ -67,6 +67,7 @@ module Spree
       end
 
       def get_products_conditions_for(base_scope, query)
+
         @solr_search = ::Sunspot.new_search(Spree::Product) do |q|
           q.keywords(query)
 
@@ -79,30 +80,77 @@ module Spree
           q.paginate(page: 1, per_page: Spree::Product.count)
         end
 
-        unless @properties[:filters].blank?
-          conditions = Spree::Sunspot::Filter::Query.new( @properties[:filters] )
-          @solr_search = conditions.build_search( @solr_search )
-        end
-
         @solr_search.build do |query|
+
+          #if not @properties[:filters].empty?
+          #  @properties[:filters].each_pair do |key, value|
+          #    filter = query.any_of do |q|
+          #      value.split("~").each do |v|
+          #        q.with(key, v)
+          #      end
+          #    end
+
+          #    query.facet(key, :exclude => filter)
+          #  end
+          #end
+
           Setup.query_filters.filters.each do |filter|
             if filter.values.any? && filter.values.first.is_a?(Range)
               query.facet(filter.search_param) do
                 filter.values.each do |value|
-                  row(value) do
+                  row(value) do 
                     with(filter.search_param, value)
                   end
                 end
               end
-            else
-              query.facet(
-                  filter.search_param,
-                  exclude: property_exclusion( filter.exclusion )
-              )
-            end
 
+              if @properties[:filters][filter.search_param.to_s]
+                range = @properties[:filters][filter.search_param.to_s].split('..').map{|d| Integer(d)}
+                query.with(filter.search_param, range[0]..range[1])
+              end
+            elsif @properties[:filters].empty? or
+                  @properties[:filters][filter.search_param.to_s].nil?
+              query.facet(filter.search_param)
+            else
+              # If we are currently filtering with this specific facet we 
+              # need to use exclude in order to allow multiple filters being 
+              # applied. TODO this should use a parameter inn order to apply
+              # this exclude only to filters with multiple set to true
+              solr_filter = query.any_of do |q|
+                values = @properties[:filters][filter.search_param.to_s]
+                values.split("~").each{|v| q.with(filter.search_param, v)}
+              end
+
+              query.facet(filter.search_param, :exclude => solr_filter)
+            end
           end
         end
+
+        #unless @properties[:filters].blank?
+        #  @conditions = Spree::Sunspot::Filter::Query.new( @properties[:filters] )
+        #  @solr_search = @conditions.build_search( @solr_search )
+        #end
+
+        #@solr_search.build do |query|
+        #  Setup.query_filters.filters.each do |filter|
+        #    if filter.values.any? && filter.values.first.is_a?(Range)
+        #      query.facet(filter.search_param) do
+        #        filter.values.each do |value|
+        #          row(value) do
+        # 
+        #            with(filter.search_param, value)
+        #          end
+        #        end
+        #      end
+        #    else
+        #      query.facet(
+        #          filter.search_param,
+        #          exclude: property_exclusion( filter.exclusion )
+        #      )
+        #    end
+        #
+        #  end
+        #end
 
         @solr_search.execute
         if @solr_search.total > 0
@@ -117,7 +165,9 @@ module Spree
 
       def prepare(params)
         super
-        @properties[:filters] = params[:s] || params['s'] || []
+        @properties[:filters] = Hash[params.reject{|k,v| not k =~ /^f_.*/}
+                                           .map{|k, v| [k.gsub("f_",""), v]}]
+
         @properties[:order_by] = params[:order_by] || params['order_by'] || []
         @properties[:total_similar_products] = params[:total_similar_products].to_i > 0 ?
             params[:total_similar_products].to_i :
@@ -125,13 +175,20 @@ module Spree
       end
 
       def property_exclusion(filter)
-        return nil if filter.blank?
-        prop = @properties[:filters].select{ |f| f == filter.to_s }
-        prop[filter] unless prop.empty?
+        return nil if filter.blank? or @conditions.nil?
+        
+        @conditions.query_params.each do |param|
+          cond = param.conditions.first
+          if cond.field_param.source.to_s == filter.to_s
+            return cond.filter
+          end
+        end
+
+        return nil
       end
 
       def ordering_property
-        @properties[:order_by] = @properties[:order_by].blank? ? %w(score desc) : @properties[:order_by].split(',')
+        @properties[:order_by] = @properties[:order_by].blank? ? %w(score desc) : @properties[:order_by].split('~')
         @properties[:order_by].flatten
       end
 
